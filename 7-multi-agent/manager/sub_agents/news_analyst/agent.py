4548920c08b56727db27f381a4a89dcd28dd1098
sjagadee <srini.jagadeesh@gmail.com>
2026-05-10 18:43:57 -0500

    Simple multi agent setup

---
+++ 7-multi-agent/manager/sub_agents/news_analyst/agent.py
@@ -0,0 +1,17 @@
+from google.adk.agents.llm_agent import Agent
+from google.adk.tools import google_search
+
+
+news_analyst = Agent(
+    model="gemini-3.1-flash-lite",
+    name='news_analyst',
+    description="News analyst agent",
+    instruction="""
+    You are a helpful assistant that can analyze news articles and provide a summary of the news.
+
+    When asked about news, you should use the google_search tool to search for the news.
+
+    If the user ask for news using a relative time, you should use the get_current_time tool to get the current time to use in the search query.
+    """,
+    tools=[google_search],
+)
